323649acb4293d7113bfc459f3e4b79b33920a6c
TeresaMontana <89273964+TeresaMontana@users.noreply.github.com>
2022-04-15 19:42:16 -0300

    probando rutas

--- proyectoIntegrador2/routes/productos.js
+++ proyectoIntegrador2/routes/productos.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 const productoController = require('../controllers/productoController');
 
-router.get('/', productoController.detalleProducto); //hay que poner lo de product add
-router.get('/', productoController.agregarProducto); //ver esto
+router.get('/detalle/:id', productoController.detalleProducto); //hay que poner lo de product add
+router.get('/agregar', productoController.agregarProducto); //ver esto
 
 
 
