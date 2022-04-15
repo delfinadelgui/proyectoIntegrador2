ba0046097676add7e902ac053a02ba2217ca4c48
TeresaMontana <89273964+TeresaMontana@users.noreply.github.com>
2022-04-15 15:58:50 -0300

    head, header y footer

--- proyectoIntegrador2/controllers/indexController.js
+++ proyectoIntegrador2/controllers/indexController.js
@@ -3,7 +3,7 @@ const usuario = require ('../db/users');
 
 const indexController = {
     home: function (req, res){
-        res.render('index', { jugadores: db.lista})
+        res.render('index', { jugadores: db.lista, title: 'prueba'})
     },
     resultados: function(req,res){
         res.render("search-results")
