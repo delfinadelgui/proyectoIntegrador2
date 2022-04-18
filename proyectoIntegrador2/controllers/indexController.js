f330a3dbe6be812f2585b8cde534c3908820f5e3
delfinadelgui <80966598+delfinadelgui@users.noreply.github.com>
2022-04-18 09:26:26 -0300

    cmabie mini cosas

--- proyectoIntegrador2/controllers/indexController.js
+++ proyectoIntegrador2/controllers/indexController.js
@@ -2,8 +2,8 @@ const db = require ('../db/jugadores');
 
 
 const indexController = { //metodo
-    home: function (req, res){
-        res.render('index', { jugadores: db.lista})
+    home: function (req, res){  //obj literal
+        res.render('index', {jugadores: db.lista})
     },
     resultados: function(req,res){
         res.render("buscar-resultados")
