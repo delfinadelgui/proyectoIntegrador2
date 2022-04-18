a294ac7f799ee388776bc55700ceca98c420cc0a
delfinadelgui <80966598+delfinadelgui@users.noreply.github.com>
2022-04-18 09:55:33 -0300

    agregue anotacion

--- proyectoIntegrador2/controllers/indexController.js
+++ proyectoIntegrador2/controllers/indexController.js
@@ -1,13 +1,13 @@
 const db = require ('../db/jugadores'); 
 
 
-const indexController = { //metodo
+const indexController = { 
     home: function (req, res){  //obj literal
-        res.render('index', {jugadores: db.lista})
+        res.render('index', {jugadores: db.lista}) //ejs al nav
     },
     resultados: function(req,res){
         res.render("buscar-resultados")
     },
 }
 
-module.exports = indexController; //importamos
+module.exports = indexController; 
